adf9fbf4c77df9f238803531b2dc32bf57e817fe
abdallah moumni <abdelrebel001@gmail.com>
2024-01-08 08:35:07 -0800

    --__--__--__--__--__--__--

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return 0, none
+    new.tuple = ()
+    if len(sentence) == 0:
+        new.tuple = 0, "None"
     else:
-        return len(sentence), sentence[0]
+        new.tuple = len(sentence), sentence[0]
+    return new.tuple
