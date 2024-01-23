7462d62701ca0dbb06943cdd0ce17a74bf146d9d
abdallah moumni <abdelrebel001@gmail.com>
2024-01-23 09:45:41 -0800

    .

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,14 @@
 
 
 class Square:
-    """ a class Square that defines a square. """
+    """ a class Square that defines a square. 
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new square with a given size.
+        area(self): Calculates and returns the area of the square.
+    """
 
     def __init__(self, size=0):
         """Initializing this square class.
