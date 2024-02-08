710d42afac804602cb0c6224ffeb9ca45c0caae1
abdallah moumni <abdelrebel001@gmail.com>
2024-02-07 19:06:23 -0800

    l

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,7 +5,7 @@ This module defines a text file insertion function
 
 
 def append_after(filename="", search_string="", new_string=""):
-"""Inserts text after each line containing a given string in a file"""
+    """Inserts text after each line containing a given string in a file"""
     result = ""
     with open(filename) as fd:
         for line in fd:
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
+"""
+This module adds all arguments to a Python list and save them to a file.
+"""
 
 
 import sys
-import os
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if os.path.exists('add_item.json') is True:
-    lista = list(load_from_json_file("add_item.json"))
-    for i in range(1, len(sys.argv)):
-        lista.append(sys.argv[i])
-    save_to_json_file(lista, "add_item.json")
-else:
-    lista = []
-    save_to_json_file(lista, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
